696732f46217ada7e9b8595640ddcf1551008819
Marcin <mgabrys422@gmail.com>
2020-03-06 16:12:55 +0100

    clear

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,35 +13,17 @@ function calcMe(x) {
     document.getElementById("Ergebnis").innerHTML += x;
 }
 
-function clear(){
-    document.getElementById("Ergebnis").innerHTML="";
+function clearME(){
+    document.getElementById("Ergebnis").innerHTML = " ";
 }
-function result(){
+function resultMe(){
     
-     document.getElementById("Ergebnis").innerHTML=eval;
-     (document.getElementById("Ergebnis").innerHTML)
- }
+    document.getElementById("Ergebnis").innerHTML=eval;
+    document.getElementById("Ergebnis").innerHTML
+  }
 
-// function that display value 
-// function dis(val) 
-//  { 
-//      document.getElementById("result").value+=val 
-// } 
-  
-// function that evaluates the digit and return result 
- function solve() 
- { 
-    let x = document.getElementById("result").value 
-    let y = eval(x) 
-    document.getElementById("result").value = y 
-} 
-  
-// function that clear the display 
- function clr() 
- { 
-     document.getElementById("result").value = "" 
-} 
 
+ 
 
 
 
