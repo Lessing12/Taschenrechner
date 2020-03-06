587f4b2e2a843672fdf7697e1051166234a59cb1
Marcin <mgabrys422@gmail.com>
2020-03-06 14:00:54 +0100

    tabelle

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+var x = 5;
+var y = 2;
+var z = x + y;
+document.getElementById("Ergebnis").innerHTML = z;
+
+
+
+
+// function that display value 
+// function rechnen() 
+//  { 
+//      document.getElementsById("Ergebnis").innerHTML = "1";
+// } 
+  
+// function that evaluates the digit and return result 
+//  function solve() 
+//  { 
+//     let x = document.getElementById("Ergebnis").value 
+//     let y = eval(x) 
+//     document.getElementById("Ergebnis").value = y 
+// } 
+  
+// // function that clear the display 
+//  function clr() 
+//  { 
+//      document.getElementById("Ergebnis").value = "" 
+// } 
